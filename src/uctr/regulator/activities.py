a0ba61d675501a9d124943f94c7ba3459e968224
KOSASIH <kosasihg88@gmail.com>
2024-05-04 00:08:34 +0700

    Create activities.py

---
+++ src/uctr/regulator/activities.py
@@ -0,0 +1,15 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def manage_activities():
+    """
+    Manages computer technology activities.
+    """
+    logger.info("Managing computer technology activities...")
+
+def check_compliance():
+    """
+    Checks compliance with regulations.
+    """
+    logger.info("Checking compliance with regulations...")
