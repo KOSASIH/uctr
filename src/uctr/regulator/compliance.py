5dee59ca639ece6e27b947f858ca935b317c023f
KOSASIH <kosasihg88@gmail.com>
2024-05-04 00:09:31 +0700

    Create compliance.py

---
+++ src/uctr/regulator/compliance.py
@@ -0,0 +1,9 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def ensure_compliance():
+    """
+    Ensures compliance with regulations.
+    """
+    logger.info("Ensuring compliance with regulations...")
